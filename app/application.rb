fed6f44854a9536efd53652d1a99546bc48f881c
John Hayes <johnnyhayes7@gmail.com>
2019-04-05 17:39:13 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+require "pry"
+class Application
+  
+  def call(env)
+    
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      if @@items.include?(item_name)
+        # binding.pry
+        item = @@items.find{|i| i.name == item_name}
+        
+        resp.write item.price
+      else
+        resp.status 400
+        resp.write "We do not have that item"
+      end
+    else
+      resp.status = 404
+      resp.write "404 Route Not Found"
+    end
+   end 
+end
