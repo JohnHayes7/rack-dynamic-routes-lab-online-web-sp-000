69ec400e5d648613d07c607ebc7848d7ae08d22b
John Hayes <johnnyhayes7@gmail.com>
2019-04-05 18:01:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     
     if req.path.match(/items/)
     item_name = req.path.split("/items/").last
-    #binding.pry
+    binding.pry
       if @@items.include?(item_name)
         end
       else
