baf9e79740dedcb9c9e57f4ac937486e5a9241f2
John Hayes <johnnyhayes7@gmail.com>
2019-04-05 17:44:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,8 +3,8 @@ class Application
   
   def call(env)
     
-    resp = Rack::Response.new 
-    req = Rack::Request.new(env)
+    # resp = Rack::Response.new 
+    # req = Rack::Request.new(env)
     
   end
 end
