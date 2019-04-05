cb56d06132a1284f74fdf726f1c2a6da76d54b38
John Hayes <johnnyhayes7@gmail.com>
2019-04-05 18:01:23 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
     item_name = req.path.split("/items/").last
     binding.pry
       if @@items.include?(item_name)
-        end
+  
       else
         resp.status = 400
         resp.write "Item not found"
