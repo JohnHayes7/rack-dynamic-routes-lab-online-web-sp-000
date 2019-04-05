8504b31b7cb352bf0dcaed17fafffa5e2e7b5aad
John Hayes <johnnyhayes7@gmail.com>
2019-04-05 17:46:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,11 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env)
     
-    if req.path.match
+    if req.path.match(/items/)
+      
+    else
+      resp.status = 404
+      resp.write "Invalid Path"
     
   end
 end
